e412bcf774c8488ca58c40e20b7d35a0c5db4b07
samsad <samali2md@gmail.com>
2021-05-18 11:44:17 +0545

    this is map pattern of nepal

---
+++ pattern.php
@@ -0,0 +1,46 @@
+<!-- <?php
+
+for($var = 1; $var<=10; $var++ ){
+
+	for($i = 10; $i >=$var; $i--){
+		echo "*","&nbsp","&nbsp";
+	}
+
+	echo "<br>";
+}
+
+?> -->
+<?php
+
+for($var = 1; $var<=10; $var++ ){
+
+	for($i = 1; $i <=$var; $i++){
+		echo "*","&nbsp","&nbsp";
+	}
+
+	echo "<br>";
+}
+
+?>
+<?php
+
+for($var = 1; $var<=10; $var++ ){
+
+	for($i = 1; $i <=$var; $i++){
+		echo "*","&nbsp","&nbsp";
+	}
+
+	echo "<br>";
+}
+
+?>
+<?php
+
+for($var = 1; $var<=10; $var++ ){
+
+		echo "*","&nbsp","&nbsp";
+
+	echo "<br>";
+}
+
+?>
